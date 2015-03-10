2d00d409eb812df7481970aafd5b3921666d9279
Kersey14 <13vs14@gmail.com>
2015-03-10 14:01:49 +0200

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,11 +4,14 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 gem 'bootstrap-sass'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails', '~> 2.14.0.rc1'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,39 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+  before_action :restrict_registration, only: [:new, :create]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show 
   	@user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    unless current_user?(user)
+      user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_url
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else 
+      render 'edit'
+    end
   end
 
   def new
@@ -23,4 +56,27 @@ class UsersController < ApplicationController
   	def user_params	
   	  params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    #BeforeFilters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        flash[:warning] = "Please sign in."
+        redirect_to signin_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
+
+    def restrict_registration
+       redirect_to root_url, notice: "You are already regsitered." if signed_in?
+    end
 end
